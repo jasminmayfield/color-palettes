5a9439567cae071da054e6c688308bb645638b63
Jasmin Mayfield <jasmin.mayfield@xero.com>
2015-01-25 19:04:30 +1300

    lighten and darken functionality good to go

--- controller.js
+++ controller.js
@@ -6,20 +6,27 @@ Controller.prototype = {
 
   start: function(){
     this.view = new View();
-    this.bindEvents();
-    this.showAllColors();
 
+    this.showAllColors();
     this.view.addColorGroup();
+
+    this.updateSortables();
+    this.bindEvents();
   },
 
   bindEvents: function(){
     $(this.view.addColorGroupButtonSelector).on("click", this.addColorGroupButton.bind(this));
+    $('.hex-code, .shade-percent').on("focusout", this.changeGroupColor.bind(this));
   },
 
   addColorGroupButton: function(e) {
     e.preventDefault();
     console.log("adding a color group");
-    this.view.addColorGroup();
+
+    var newGroup = this.view.addColorGroup();
+    $('.hex-code, .shade-percent', newGroup).on("focusout", this.changeGroupColor.bind(this));
+    this.updateSortables();
+
   },
 
   showAllColors: function() {
@@ -37,6 +44,52 @@ Controller.prototype = {
 
     });
 
+  },
+
+  changeGroupColor: function(e) {
+    var group = $(e.target).parent('.group');
+    var hexCode = $('.hex-code',group).text();
+    var shadePercent = $('.shade-percent',group).text();
+    this.updateGroupColor(group, hexCode, shadePercent);
+  },
+
+  updateGroupColor: function(group, hexCode, shadePercent) {
+    group.css('background-color', this.shadeColor(hexCode, shadePercent));
+  },
+
+  updateSortables: function() {
+    $(".connectedSortable").sortable({
+      connectWith: ".connectedSortable",
+      helper: 'clone',
+      appendTo: 'body'
+    }).disableSelection();
+  },
+
+  shadeColor: function(color, amt) {
+    var useHash = false;
+    if (color[0] == "#") {
+      color = color.slice(1);
+      useHash = true;
+    }
+
+    var num = parseInt(color,16);
+    var amt = parseInt(amt);
+    var r = (num >> 16) + amt;
+
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (useHash?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
   }
 
 };
--- view.js
+++ view.js
@@ -2,6 +2,7 @@ function View() {
   this.addColorGroupButtonSelector = "#add-color-group-button";
   this.colorListSelector = "#color-list";
   this.groupListSelector = "#group-list";
+  this.connectedSortablesSelector = ".connectedSortable";
 }
 
 View.prototype = {
@@ -16,23 +17,13 @@ View.prototype = {
     var group = $('<div></div>');
     group.addClass('group');
     group.append('<div contenteditable="true" class="hex-code">#c3325f</div>');
+    group.append('<div contenteditable="true" class="shade-percent">0</div>');
     group.css('background-color', '#c3325f');
 
     group.append('<div class="color-match-list connectedSortable"></div>');
 
     $(this.groupListSelector).append(group);
-
-    $('.hex-code',group).on("focusout", function(e) {
-      console.log("entering hex");
-      $(e.target).parent('.group').css('background-color', $(e.target).text());
-    });
-
-    $('.connectedSortable').sortable({
-      connectWith: ".connectedSortable",
-      helper: 'clone',
-      appendTo: 'body'
-    }).disableSelection();
-
+    return group;
   }
 
   // Adjustment is lighten and darken by a percentage input only
