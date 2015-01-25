022fef91ed8486bc3ddeb829fa1944256c07a174
Jasmin Mayfield <jasmin.mayfield@xero.com>
2015-01-25 15:06:51 +1300

    background color updating with corresponding hex value text input

--- view.js
+++ view.js
@@ -15,12 +15,18 @@ View.prototype = {
 
     var group = $('<div></div>');
     group.addClass('group');
-    group.append('<h2 contenteditable="true">'+"New Group"+'</h2>');
+    group.append('<div contenteditable="true" class="hex-code">#c3325f</div>');
+    group.css('background-color', '#c3325f');
+
     group.append('<div class="well"></div>');
     $('.well',group).addClass('connectedSortable');
 
-
     $(this.groupListSelector).append(group);
+
+    $('.hex-code').on("focusout", function() {
+      console.log("entering hex");
+      $('.hex-code').parent('.group').css('background-color', $('.hex-code').text());
+    });
 
     $('.connectedSortable').sortable({
       connectWith: ".connectedSortable"
